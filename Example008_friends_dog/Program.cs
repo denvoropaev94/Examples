fed2ad62605a30520816437f2c8693bdc2c35315
«Denis» <denvoropaev7@gmail.com>
2022-08-05 12:15:54 +0300

    Example 008 zadacha friends and dog

---
+++ Example008_friends_dog/Program.cs
@@ -0,0 +1,25 @@
+﻿int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int count = 0;
+int time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    if (friend == 2)
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;   
+    }
+distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+count++;
+Console.WriteLine(count);
+}
+
