c01e800a49592870b47cb217e890d381bcea0315
«Denis» <denvoropaev7@gmail.com>
2022-08-08 10:39:53 +0300

    Example 010 method array

---
+++ Example010_method_array/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {21,55,674,21,90,76,33};
+int n = array.Length;
+int find = 21;
+int index = 0;
+
+while (index < n){
+    if(array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+    index++;
+}
