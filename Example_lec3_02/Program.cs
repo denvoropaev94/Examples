b29bc3402494658c236204a56dfee311fad9727e
«Denis» <denvoropaev7@gmail.com>
2022-08-18 10:40:33 +0300

    Заполнил массив 8 элементами, вывел на экран,и сделал сортировку массива с макс до мин элемента

---
+++ Example_lec3_02/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] arr = new int[8];
+
+  int[] FillArray(int[] array){
+  for(int i = 0;i<array.Length;i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t ");
+                array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+  }
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }
+
+
+        int temp = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temp;
+    }
+}
+
+int[] n= FillArray(arr);
+PrintArray(n);
+
+SelectionSort(arr);
+
+PrintArray(arr);
